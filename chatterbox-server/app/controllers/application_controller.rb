aec3d2a4b0c978ae9cf1f3c3e807a0ac3989c6eb
Tucker Steil <tuckersteil@gmail.com>
2022-10-13 12:56:24 -0700

    commit tree 48778e53b8ff417f9056d057209161a1284cf090

--- chatterbox-server/app/controllers/application_controller.rb
+++ chatterbox-server/app/controllers/application_controller.rb
@@ -1,6 +1,29 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-  # Add routes
-  
+  get '/messages' do 
+    messages = Message.all.order(created_at: :asc)
+    messages.to_json
+  end 
+
+  post '/messages' do
+  new_message = Message.create(
+    body: params[:body],
+    username: params[:username]
+  ) 
+  new_message.to_json
+  end 
+
+  patch '/messages/:id' do 
+    update_message = Message.find(params[:id])
+    update_message.update(
+      body: params[:body]
+    )
+    update_message.to_json
+  end 
+
+  delete '/messages/:id' do 
+    delete_message = Message.find(params[:id])
+    delete_message.destroy 
+  end 
 end
